3f4d404e20912ee7b6e7bcd06f4b167b74913084
ryan-becwar <rbecwar1@gmail.com>
2015-04-06 22:23:59 -0400

    Basic GUI building

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,5 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Ryan on 4/6/2015.
  */
-public class GameFrame {
+public class GameFrame extends JFrame {
+
+    private JPanel buttonPanel;
+    private JButton stepButton;
+    private static GameFrame gameFrameInstance;
+
+    private GameFrame(String name) {
+        super(name);
+
+        setSize(800, 400);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+        buttonPanel = new JPanel();
+        add(buttonPanel);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+
+        stepButton = new JButton("Step");
+        buttonPanel.add(stepButton);
+
+
+
+
+    }
+
+    public static GameFrame getInstance(String name) {
+        if(gameFrameInstance == null)
+           gameFrameInstance = new GameFrame(name);
+        return gameFrameInstance;
+    }
+
 }
---
+++ src/MainDriver.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Ryan on 4/6/2015.
+ */
+public class MainDriver {
+    public static void main(String args[]){
+        GameFrame window = GameFrame.getInstance("Conway's Game of Life");
+        window.setVisible(true);
+
+    }
+}
