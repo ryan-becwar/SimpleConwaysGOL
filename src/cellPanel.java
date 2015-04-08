803aa700ee7c924de974110f7e2579971978b74f
ryan-becwar <rbecwar1@gmail.com>
2015-04-08 18:14:14 -0400

    Started Grid Building

---
+++ src/cellPanel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by Ryan on 4/8/2015.
+ */
+public class cellPanel extends JPanel {
+    private Boolean curState;
+    private Boolean updatedState;
+    
+}
---
+++ src/gridPanel.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Ryan on 4/8/2015.
+ */
+public class gridPanel extends JPanel {
+
+    private GridBagConstraints gbConst;
+    private
+
+    public gridPanel(int width, int height, int cellPixWidth) {
+        this.setLayout(new GridBagLayout());
+        gbConst = new GridBagConstraints();
+
+
+    }
+
+    public void
+}
